36c15586d8a04db71cb4d6ab24d94a0d5bba1f2f
Karl Palmskog <palmskog@gmail.com>
2017-07-24 17:47:57 -0500

    Travis files

---
+++ .travis-ci.sh
@@ -0,0 +1,10 @@
+set -ev
+
+opam init --yes --no-setup
+eval $(opam config env)
+
+opam repo add coq-released https://coq.inria.fr/opam/released
+
+opam pin add coq $COQ_VERSION --yes --verbose
+
+opam pin add reglang . --yes --verbose
